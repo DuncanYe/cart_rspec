1e2c442ec265ed5569617e092b8c0d81c028460f
Duncan <loxa10077@gmail.com>
2018-05-24 12:12:18 +0800

    add test 3

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -20,5 +20,18 @@ RSpec.describe Cart, type: :model do
       expect(cart.items.second.quantity).to be 5
     end
 
+    it "商品可放進購物車，也可以拿出來(要是同個商品)" do
+      cart = Cart.new
+      p1 = Product.create(title:"拉拉鍊")
+      p2 = Product.create(title:"游泳")
+
+      4.times { cart.add_item(p1.id) }
+      3.times { cart.add_item(p2.id) }
+
+      expect(cart.items.first.title).to eq "拉拉鍊"
+      expect(cart.items.second.product_id).to eq p2.id
+      expect(cart.items.first.product).to be_a Product
+    end
+
   end
 end
