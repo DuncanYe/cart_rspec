8720b9ffb94b15325f6888d874172140ef80d128
Duncan <loxa10077@gmail.com>
2018-05-24 09:59:21 +0800

    add test 1

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
   
+  describe "購物車基本功能" do 
+
+    it "商品可放進購物車，購物車裡要有東西" do
+      cart = Cart.new
+      cart.add_item (1)
+      expect(cart.empty?).to be false
+    end
+  end
 end
